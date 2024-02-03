7573ff6563dfed495d47d7b99ef26a05e5c4ee8a
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-03 21:12:50 +0300

    ..

---
+++ SignalRApi/DAL/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
