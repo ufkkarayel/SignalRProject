194a6f2a69d5016bbf2021b499cfc07464b59bc2
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-03 21:29:17 +0300

    ..

---
+++ SignalR.EntityLayer/Entities/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
