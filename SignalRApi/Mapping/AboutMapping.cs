757de33a26826fd4c21ebfbb8d046f26fa874276
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-04 19:56:18 +0300

    ..

--- SignalRApi/Mapping/AboutMapping.cs
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Mapping
 {
@@ -6,7 +8,10 @@ namespace SignalRApi.Mapping
     {
         public AboutMapping()
         {
-              
+            CreateMap<About, ResultAboutDto>().ReverseMap();
+            CreateMap<About, CreateAboutDto>().ReverseMap();
+            CreateMap<About,UpdateAboutDto>().ReverseMap();
+            CreateMap<About,GetAboutDto>().ReverseMap();
         }
     }
 }
