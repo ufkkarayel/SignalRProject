eeef3ce539945e219eb21db02f705e03501f4646
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-04 19:53:22 +0300

    ..

---
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace SignalRApi.Mapping
+{
+    public class AboutMapping  : Profile
+    {
+        public AboutMapping()
+        {
+              
+        }
+    }
+}
