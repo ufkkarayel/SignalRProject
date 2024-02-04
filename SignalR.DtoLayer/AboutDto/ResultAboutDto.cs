631acef2fdb35220ace23a4aa5acdbeb4f6b0459
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-04 19:07:11 +0300

    ..

--- SignalR.DtoLayer/AboutDto/ResultAboutDto.cs
+++ SignalR.DtoLayer/AboutDto/ResultAboutDto.cs
@@ -8,5 +8,9 @@ namespace SignalR.DtoLayer.AboutDto
 {
     public class ResultAboutDto
     {
+        public int AboutID { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 }
--- SignalR.EntityLayer/Entities/About.cs
+++ SignalR.EntityLayer/Entities/About.cs
@@ -4,6 +4,7 @@
     {
         public int AboutID { get; set; }
         public string ImageUrl { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
     }
 }
