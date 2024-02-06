d16dbda990b59ce045b563ffdb43e0a30d8c6bb4
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-06 21:50:03 +0300

    .

--- SignalRApi/Controllers/AboutController.cs
+++ SignalRApi/Controllers/AboutController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -24,8 +25,40 @@ namespace SignalRApi.Controllers
         [HttpPost]
         public IActionResult CreateAbout(CreateAboutDto createAboutDto)
         {
-            _aboutService.TAdd(createAboutDto);
+            About about = new About()
+            {
+                Title = createAboutDto.Title,
+                Description = createAboutDto.Description,
+                ImageUrl = createAboutDto.ImageUrl
+            };
+            _aboutService.TAdd(about);
             return Ok("Hakkımda kısmı başarılı bir şekilde eklendi.");
+        }
+        [HttpDelete]
+        public IActionResult DeleteAbout(int id)
+        {
+            var value = _aboutService.TGetByID(id);
+            _aboutService.TDelete(value);
+            return Ok("Hakkımda alanı silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            About about = new About()
+            {
+                AboutID = updateAboutDto.AboutID,
+                Title = updateAboutDto.Title,
+                Description = updateAboutDto.Description,
+                ImageUrl = updateAboutDto.ImageUrl
+            };
+            _aboutService.TUpdate(about);
+            return Ok("Hakkımda alanı güncellendi");
+        }
+        [HttpGet("GetAbout")]
+        public IActionResult GetAbout(int id)
+        {
+           var value= _aboutService.TGetByID(id);
+            return Ok(value);
         }
     }
 }
