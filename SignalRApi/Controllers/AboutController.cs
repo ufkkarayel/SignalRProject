812acdd0ef8300d6614b51d602ce34df3ca0f5d8
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-06 21:43:49 +0300

    ..

--- SignalRApi/Controllers/AboutController.cs
+++ SignalRApi/Controllers/AboutController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.AboutDto;
 
 namespace SignalRApi.Controllers
 {
@@ -7,5 +9,23 @@ namespace SignalRApi.Controllers
     [ApiController]
     public class AboutController : ControllerBase
     {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+        [HttpGet]
+        public IActionResult AboutList()
+        {
+            var values= _aboutService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            _aboutService.TAdd(createAboutDto);
+            return Ok("Hakkımda kısmı başarılı bir şekilde eklendi.");
+        }
     }
 }
