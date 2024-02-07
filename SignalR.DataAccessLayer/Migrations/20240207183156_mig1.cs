bc4797dc3bea0c4225b67e4bb003eaae69a8d072
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-07 21:32:13 +0300

    ..

---
+++ SignalR.DataAccessLayer/Migrations/20240207183156_mig1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignalR.DataAccessLayer.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Abouts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Abouts");
+        }
+    }
+}
