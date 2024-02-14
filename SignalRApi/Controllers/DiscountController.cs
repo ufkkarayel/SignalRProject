207e61b5dfc21f9c213e1bfce9e7a1a3b8f0263d
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-14 21:51:18 +0300

    ..

---
+++ SignalRApi/Controllers/DiscountController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IMapper _mapper;
+    }
+}
