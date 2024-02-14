498e7fbf9affb044dcdbad06234d15df8d2b8fac
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-14 21:59:30 +0300

    ...

--- SignalRApi/Controllers/DiscountController.cs
+++ SignalRApi/Controllers/DiscountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.DtoLayer.DiscountDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -23,6 +24,18 @@ namespace SignalRApi.Controllers
         {
             var value = _mapper.Map<List<ResultDiscountDto>>(_discountService.TGetListAll());
             return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateDiscount(CreateDiscountDto createDiscountDto)
+        {
+            _discountService.TAdd(new Discount
+            {
+                Amount = createDiscountDto.Amount,
+                Description = createDiscountDto.Description,
+                ImageUrl = createDiscountDto.ImageUrl,
+                Title = createDiscountDto.Title
+            });
+            return Ok("Başarıyla oluşturuldu");
         }
     }
 }
