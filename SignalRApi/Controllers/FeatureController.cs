ac2aceff6d1e912381aa67ba04fa9b1cbbbdd008
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-16 21:34:43 +0300

    ..

---
+++ SignalRApi/Controllers/FeatureController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureController : ControllerBase
+    {
+        private readonly IFeatureService _featureService;
+        private readonly IMapper _mapper;
+
+        public FeatureController(IFeatureService featureService, IMapper mapper)
+        {
+            _featureService = featureService;
+            _mapper = mapper;
+        }
+    }
+}
