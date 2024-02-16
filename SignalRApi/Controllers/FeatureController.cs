d9d05dc419f0dc43429492105d3e13f21af04378
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-16 21:47:38 +0300

    ..

--- SignalRApi/Controllers/FeatureController.cs
+++ SignalRApi/Controllers/FeatureController.cs
@@ -52,5 +52,20 @@ namespace SignalRApi.Controllers
             var value = _featureService.TGetByID(id);
             return Ok(value);
         }
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            _featureService.TUpdate(new Feature
+            {
+                Description1 = updateFeatureDto.Description1,
+                Description2 = updateFeatureDto.Description2,
+                Description3 = updateFeatureDto.Description3,
+                FeatureID = updateFeatureDto.FeatureID,
+                Title1 = updateFeatureDto.Title1,
+                Title2 = updateFeatureDto.Title2,
+                Title3 = updateFeatureDto.Title3
+            });
+            return Ok("Başarıyla güncellendi");
+        }
     }
 }
