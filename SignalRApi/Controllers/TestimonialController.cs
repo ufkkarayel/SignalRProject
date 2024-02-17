5a1d3974f15913aa648b2814f24649e62d56e20d
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-17 21:22:49 +0300

    ..

---
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+        private readonly IMapper _mapper;
+
+    }
+}
