028c05ef4e6614e97debba58442159a8fc29579a
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-17 21:23:59 +0300

    ..

--- SignalRApi/Controllers/TestimonialController.cs
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.TestimonialDto;
 
 namespace SignalRApi.Controllers
 {
@@ -12,5 +13,16 @@ namespace SignalRApi.Controllers
         private readonly ITestimonialService _testimonialService;
         private readonly IMapper _mapper;
 
+        public TestimonialController(ITestimonialService testimonialService, IMapper mapper)
+        {
+            _testimonialService = testimonialService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values = _mapper.Map<List<ResultTestimonialDto>>(_testimonialService.TGetListAll());
+            return Ok(values);
+        }
     }
 }
