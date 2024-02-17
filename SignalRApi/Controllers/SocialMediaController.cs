e9ed8cd826b8c473bb23fc15f1bf382d33c88fdd
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-17 21:09:03 +0300

    ..

--- SignalRApi/Controllers/SocialMediaController.cs
+++ SignalRApi/Controllers/SocialMediaController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.SocialMediaDto;
 
 namespace SignalRApi.Controllers
 {
@@ -17,6 +18,11 @@ namespace SignalRApi.Controllers
             _service = service;
             _mapper = mapper;
         }
-
+        [HttpGet]
+        public IActionResult SocialMediaList()
+        {
+            var values = _mapper.Map<List<ResultSocialMediaDto>>(_service.TGetListAll());
+            return Ok(values);
+        }
     }
 }
