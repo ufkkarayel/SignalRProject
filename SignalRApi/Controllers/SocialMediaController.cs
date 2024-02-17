de322bebeb8949ecc73973caaa9bb4d0430a2028
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-17 21:07:16 +0300

    ..

---
+++ SignalRApi/Controllers/SocialMediaController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediaController : ControllerBase
+    {
+        private readonly ISocialMediaService _service;
+        private readonly IMapper _mapper;
+
+        public SocialMediaController(ISocialMediaService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+    }
+}
