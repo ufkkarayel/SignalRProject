baabab446ac68f8ea86bfc4513c3f7001ce00673
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-17 21:20:19 +0300

    ..

--- SignalRApi/Controllers/SocialMediaController.cs
+++ SignalRApi/Controllers/SocialMediaController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.DtoLayer.SocialMediaDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -24,5 +25,42 @@ namespace SignalRApi.Controllers
             var values = _mapper.Map<List<ResultSocialMediaDto>>(_service.TGetListAll());
             return Ok(values);
         }
+        [HttpPost]
+        public IActionResult CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
+        {
+            _service.TAdd(new SocialMedia()
+            {
+                Icon = createSocialMediaDto.Icon,
+                Title = createSocialMediaDto.Title,
+                Url = createSocialMediaDto.Url
+            });
+            return Ok("Başarıyla kaydedildi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var value=_service.TGetByID(id);
+            _service.TDelete(value);
+            return Ok("Başarıyla silindi");
+        }
+        [HttpGet("GetSocialMedia")]
+        public  IActionResult GetSocialMedia(int id)
+        {
+            var value =_service.TGetByID(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            _service.TUpdate(new SocialMedia()
+            {
+                Icon = updateSocialMediaDto.Icon,
+                Title = updateSocialMediaDto.Title,
+                Url = updateSocialMediaDto.Url,
+                SocialMediaID = updateSocialMediaDto.SocialMediaID
+            });
+            return Ok("Başarıyla kaydedildi");
+        }
+
     }
 }
