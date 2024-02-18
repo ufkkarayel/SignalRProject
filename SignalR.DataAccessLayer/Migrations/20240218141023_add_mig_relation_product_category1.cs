a78e2767bfc9ea81ab91501274ea8e9f2f28e6e7
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-18 17:17:21 +0300

    ..

---
+++ SignalR.DataAccessLayer/Migrations/20240218141023_add_mig_relation_product_category1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignalR.DataAccessLayer.Migrations
+{
+    public partial class add_mig_relation_product_category1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategorID",
+                table: "Products");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategorID",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
