23ed25d008add99f33456736daad4c0248e045aa
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-25 19:21:03 +0300

    ..

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -25,5 +25,10 @@ namespace SignalRWebUI.Controllers
             }
             return View();
         }
+
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
     }
 }
