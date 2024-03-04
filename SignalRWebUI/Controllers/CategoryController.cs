95ce5762c7d43f77cf55b857c97f113dfe6b0c13
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-03-04 22:59:03 +0300

    ...

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -56,6 +56,7 @@ namespace SignalRWebUI.Controllers
             }
             return View();
         }
+        [HttpGet]
         public async Task<IActionResult> UpdateCategory(int id)
         {
             var client = _httpClientFactory.CreateClient();
@@ -65,6 +66,19 @@ namespace SignalRWebUI.Controllers
                 var jsonData= await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent= new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7152/api/Category", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
